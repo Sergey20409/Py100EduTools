60e1a82379c146684f12bc6c21a998792d770179
sergzah <traktorsergey@gmail.com>
2024-11-08 21:00:16 +0300

    Part 3_1

--- Занятие 2. Условия и циклы (1)/Лабораторные работы/Продолжение финансового планирования/main.py
+++ Занятие 2. Условия и циклы (1)/Лабораторные работы/Продолжение финансового планирования/main.py
@@ -2,14 +2,13 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
-money_capital=0
+money_capital = 0
 
-for months in range (months):
+for months in range(months):
 
-    money_capital=money_capital+spend-salary
+    money_capital = money_capital + spend - salary
     spend = spend * (1 + increase)
-    months+=1
-    if months ==10:
-# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+    months += 1
+    if months == 10:
+        # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
         print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", f"{money_capital:.2f}")
-
--- Занятие 3. Функции и методы (1)/Функции/Передача аргументов в функцию и возвращаемые значения/main.py
+++ Занятие 3. Функции и методы (1)/Функции/Передача аргументов в функцию и возвращаемые значения/main.py
@@ -1,4 +1,14 @@
 # TODO Напишите функцию `multiply_numbers`
+def multiply_numbers(a, b):
+    return a * b
 
+
+product1 = multiply_numbers(2, 3)
+product2 = multiply_numbers(4, 5)
+product3 = multiply_numbers(6, 7)
+
+print(product1)
+print(product2)
+print(product3)
 
 # TODO Перемножьте с помощью функции `multiply_numbers` три пары чисел и распечатайте результаты
