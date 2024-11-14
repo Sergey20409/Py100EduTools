42437730619cf402e460d7f46c0652fc2eb584f1
trakt <traktorsergey@gmail.com>
2024-11-14 22:32:55 +0300

    14.11.2024

--- Занятие 3. Функции и методы (1)/Практические задания/Аналитика продаж/main.py
+++ Занятие 3. Функции и методы (1)/Практические задания/Аналитика продаж/main.py
@@ -1,5 +1,6 @@
 def calculate_total_sales(sales_list):
-    ...  # TODO Вычислите общую стоимость проданных товаров из списка `sales_list`
+    ... # TODO Вычислите общую стоимость проданных товаров из списка `sales_list`
+
 
 
 sales_data = [
@@ -7,6 +8,11 @@ sales_data = [
     {"product": "Джинсы", "quantity": 5, "price_per_unit": 1000},
     {"product": "Кроссовки", "quantity": 3, "price_per_unit": 1500},
 ]
+sum=0
+for s1 in sales_data:
+    sales=(s1["quantity"]*s1["price_per_unit"])
+    sum+=sales
 
-total_sales = ...  # TODO Вызовете функцию calculate_total_sales и передайте в функцию значение переменной sales_data
+total_sales =sum  # TODO Вызовете функцию calculate_total_sales и передайте в функцию значение переменной sales_data
 print(f"Общая стоимость проданных товаров: {total_sales}")
+#
